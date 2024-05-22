4e9339d88ed37e143e7ae36a6e61b326d0768099
SMHrvoje <hs53166@fer.hr>
2024-05-22 17:35:55 +0200

    update of gradilisteController docs, now this one is an example for how to doc a controller

--- src/main/java/baumit/controllers/GradilisteController.java
+++ src/main/java/baumit/controllers/GradilisteController.java
@@ -2,15 +2,20 @@ package baumit.controllers;
 
 import baumit.dtos.GradilisteWithTasksDto;
 import baumit.services.GradilisteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Tag(name = "Construction Controller", description = "APIs related to managing specific construction")
 @RequiredArgsConstructor
 @RequestMapping("/gradiliste")
 public class GradilisteController {
@@ -18,7 +23,12 @@ public class GradilisteController {
 
 
     @GetMapping("/{id}")
-    public String jednoGradiliste(Model model, @PathVariable int id){
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved construction"),
+            @ApiResponse(responseCode = "404", description = "Construction not found")
+    })
+    @Operation(summary = "api for getting one specific construction depending on the id passed in the url")
+    public String jednoGradiliste(Model model,@Parameter(description = "id of the construction that will be displayed",example = "1") @PathVariable int id){
         GradilisteWithTasksDto gradiliste = gradilisteService.constructionWithTasksDtos(id);
         System.out.println(gradiliste.zadaci());
         model.addAttribute("construction", gradiliste);
